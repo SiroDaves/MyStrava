3f35298223deb71455afbd5b7cbd50dc521ba7e5
Siro Daves <sirodevske@gmail.com>
2024-05-02 08:33:45 +0300

    workouts fixes

--- app/src/main/java/com/siro/mystrava/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/com/siro/mystrava/presentation/navigation/AppNavHost.kt
@@ -21,7 +21,7 @@ fun AppNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Routes.UPLOAD
+        startDestination = Routes.HOME
     ) {
 
         composable(Routes.HOME) {
